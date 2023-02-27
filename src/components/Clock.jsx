b273667fcbef17247ed366ec7d94bd46ebfd4416
M. R. Hossain <rez182@gmail.com>
2023-02-26 23:47:34 -0500

    convert clock class to function

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,43 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import Moment from 'react-moment'
 
-export default class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date() };
-        // this.show = props.show;
+export default function Clock() {
+
+    const [dateTime, setDateTime] = useState(new Date())
+    const [fullDate, setFullDate] = useState(<Moment format='dddd, MMMM D Y'>{dateTime}</Moment>)
+    const [time, setTime] = useState(dateTime.toLocaleTimeString())
+
+    function tick() {
+        setDateTime(new Date());
+        setFullDate(<Moment format='dddd, MMMM D Y'>{dateTime}</Moment>)
+        setTime(dateTime.toLocaleTimeString())
+        clearInterval(clockID)
     }
 
-    componentDidMount() {
-        this.clockID = setInterval(() => this.tick(), 1000);
-    }
+    const clockID = setInterval(() => tick(), 1000);
 
-    componentWillUnmount() {
-        clearInterval(this.clockID);
-    }
-
-    tick() {
-        this.setState({ date: new Date() });
-    }
-
-    // NOT SURE HOW TO MAKE THIS WORK
-    getDate() {
-        return this.state.date;
-    }
-
-    render() {
-        const fullDate = <Moment format='dddd, MMMM D Y'>{this.state.date}</Moment>
-        const time = this.state.date.toLocaleTimeString()
-        return (
-            <section className=' flex flex-col pt-5 text-2xl'>
-                <div className="m-auto">
-                    {fullDate}
-                </div>
-                {/* <br /> */}
-                <div className="m-auto">
-                    {time}
-                </div>
-            </section>
-        );
-    }
+    return (
+        <section className=' flex flex-col pt-5 text-2xl'>
+            <div className="m-auto">
+                {fullDate}
+            </div>
+            <div className="m-auto">
+                {time}
+            </div>
+        </section>
+    );
 }
