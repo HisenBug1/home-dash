2100596947b90b496bee65cd47c337206849b669
M. R. Hossain <rez182@gmail.com>
2023-03-05 18:22:22 -0500

    spell correction

--- src/components/Garbage.jsx
+++ src/components/Garbage.jsx
@@ -4,14 +4,14 @@ import axios from "axios";
 const url = 'http://ubuntu:8000/garbage'
 
 export default function Garbage() {
-    const [garbage_date, setData] = useState([])
+    const [garbage_date, setDate] = useState([])
     const [garbage_type, setType] = useState()
 
     useEffect(() => {
         async function fetchData() {
             try {
                 const res = await axios.get(url)
-                setData(res.data.date)
+                setDate(res.data.date)
                 setType(res.data.type)
             } catch (err) {
                 console.log(err)
