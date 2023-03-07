7e9c449867a70f08db70599dbdd75a69b4500d2c
M. R. Hossain <rez182@gmail.com>
2023-03-07 02:12:41 -0500

    show logo

--- src/components/Garbage.jsx
+++ src/components/Garbage.jsx
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from "react"
 import axios from "axios";
 import Box from '@mui/material/Box';
 // import Button from '@mui/material/Button';
+import garbageLogo from '../images/garbage-bin-recycle-bin-svgrepo-com.png'
 
 const url = 'http://ubuntu:8000/garbage'
 
 export default function Garbage() {
-    const [garbage_date, setDate] = useState([])
+    const [garbage_date, setDate] = useState()
     const [garbage_type, setType] = useState()
+    // const [logo, setLogo] = useState(process.env.PUBLIC_URL + '/garbage-bin-recycle-bin-svgrepo-com.png')
+    const [logo, setLogo] = useState()
 
     useEffect(() => {
         async function fetchData() {
@@ -15,12 +18,15 @@ export default function Garbage() {
                 const res = await axios.get(url)
                 setDate(res.data.date)
                 setType(res.data.type)
+                if (garbage_type.toLowerCase().includes('both')) {
+                    setLogo(garbageLogo)
+                }
             } catch (err) {
                 console.log(err)
             }
         }
         fetchData();
-    }, []);
+    }, [garbage_type]);
 
     return (
         <Box className='flex flex-col rounded-[2rem] justify-items-center text-center'
@@ -35,7 +41,8 @@ export default function Garbage() {
             }}
         >
         <div>{garbage_date}</div>
-        <div>{garbage_type}</div>
+        {/* <div>{garbage_type}</div> */}
+        <img src={logo} alt='logo'/>
         </Box>
     )
 }
