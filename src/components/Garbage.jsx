055c4d401c541e7b32a3f06f9f0fe4c5b7783929
M. R. Hossain <rez182@gmail.com>
2023-03-11 20:39:00 -0500

    display appropriate logo for each type
    singe bin for waste only, double bin for both recycle and waste

--- src/components/Garbage.jsx
+++ src/components/Garbage.jsx
@@ -3,14 +3,14 @@ import axios from "axios";
 import Box from '@mui/material/Box';
 // import Button from '@mui/material/Button';
 import garbageLogo from '../images/garbage-bin-recycle-bin-svgrepo-com.png'
+import recycleLogo from '../images/recycle-svgrepo-com.png'
 
 const url = 'http://ubuntu:8000/garbage'
 
 export default function Garbage() {
     const [garbage_date, setDate] = useState()
     const [garbage_type, setType] = useState()
-    // const [logo, setLogo] = useState(process.env.PUBLIC_URL + '/garbage-bin-recycle-bin-svgrepo-com.png')
-    const [logo, setLogo] = useState()
+    const [both, setBoth] = useState()
 
     useEffect(() => {
         async function fetchData() {
@@ -18,9 +18,10 @@ export default function Garbage() {
                 const res = await axios.get(url)
                 setDate(res.data.date)
                 setType(res.data.type)
-                if (garbage_type.toLowerCase().includes('both')) {
-                    setLogo(garbageLogo)
-                }
+                if (garbage_type.toLowerCase().includes('both'))
+                    setBoth(true)
+                else
+                    setBoth(false)
             } catch (err) {
                 console.log(err)
             }
@@ -28,21 +29,45 @@ export default function Garbage() {
         fetchData();
     }, [garbage_type]);
 
-    return (
-        <Box className='flex flex-col rounded-[2rem] justify-items-center text-center'
-            sx={{
-                width: 300,
-                height: 300,
-                backgroundColor: 'primary.dark',
-                '&:hover': {
-                    backgroundColor: 'primary.main',
-                    opacity: [0.9, 0.8, 0.7],
-                },
-            }}
-        >
-        <div>{garbage_date}</div>
-        {/* <div>{garbage_type}</div> */}
-        <img src={logo} alt='logo'/>
-        </Box>
-    )
+    if (both) {
+        return (
+            <Box className='flex flex-col rounded-[2rem] justify-items-center text-center'
+                sx={{
+                    width: 200,
+                    height: 200,
+                    backgroundColor: 'primary.dark',
+                    '&:hover': {
+                        backgroundColor: 'primary.main',
+                        opacity: [0.9, 0.8, 0.7],
+                    },
+                }}
+            >
+                <div className="pt-5">{garbage_date}</div>
+                <div className="max-w-[50%] m-auto flex flex-row justify-center">
+                    <img src={garbageLogo} alt='logo of waste bin' />
+                    <img src={recycleLogo} alt='logo of recycle bin' />
+                </div>
+            </Box>
+        )
+    }
+    else {
+        return (
+            <Box className='flex flex-col rounded-[2rem] justify-items-center text-center'
+                sx={{
+                    width: 200,
+                    height: 200,
+                    backgroundColor: 'primary.dark',
+                    '&:hover': {
+                        backgroundColor: 'primary.main',
+                        opacity: [0.9, 0.8, 0.7],
+                    },
+                }}
+            >
+                <div className="pt-1">{garbage_date}</div>
+                <div className="max-w-[75%] m-auto">
+                    <img src={garbageLogo} alt='logo of waste bin' />
+                </div>
+            </Box>
+        )
+    }
 }
