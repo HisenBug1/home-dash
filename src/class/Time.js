de3e496ef8d8d55c4dce5d36184177e80a610f0b
M. R. Hossain <rez182@gmail.com>
2023-03-19 00:04:02 -0400

    Bugfix

--- src/class/Time.js
+++ src/class/Time.js
@@ -11,9 +11,9 @@ export default class Time {
 
     getDayAndDate() {
         const d = this.time
-        const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
         const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        let date = days[d.getDay()-1]+", "+months[d.getMonth()]+" "+d.getDate()+", "+d.getFullYear()
+        let date = days[d.getDay()]+", "+months[d.getMonth()]+" "+d.getDate()+", "+d.getFullYear()
         return date
     }
 
